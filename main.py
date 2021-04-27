a8e258bb972f675310b0d6fe709994dcd98aff3a
siddharth2016 <siddharthchandragzb@gmail.com>
2021-04-27 19:08:42 +0530

    add block

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import pygame
 from pygame.locals import *
 
 if __name__ == "__main__":
-    
     pygame.init()
-    pygame.display.set_mode((1000, 500))
+    
+    surface = pygame.display.set_mode((1000, 500))
+    
+    block = pygame.image.load("resources/block.jpeg")
+    block_x = 100
+    block_y = 100
+    surface.blit(block, (block_x, block_y))
+    
+    pygame.display.flip()
     
     running = True
     
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                if event == K_ESCAPE:
+                if event.key == K_ESCAPE:
                     running = False
             if event.type == QUIT:
                 running = False
